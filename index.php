ec6e64c73543da6bf1836045b24ae2f25b957a21
Tsubasa-hub <57342693+Tsubasa-hub@users.noreply.github.com>
2019-12-08 14:48:16 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <meta charset="utf-8">
+  <head>
+    <title>掲示板</title>
+   <link rel="stylesheet" href="css/styles.css">
+    
+  </head>
+  <body>
+  <header>
+    <h1>課題掲示板</h1>
+  </header>
+</body>
+</html>
+
+<?php
+echo"hello from the Top!";
+?>
